53cc558c8b671e8f00766ff7332c487d72372819
Ednagea Almira <ednagea.almira@gmail.com>
2021-02-06 09:45:02 +0700

    Update uploadtemplate.php

--- application/views/adminpusat/uploadtemplate.php
+++ application/views/adminpusat/uploadtemplate.php
@@ -4,6 +4,16 @@
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <?= form_open_multipart('adminpusat/uploadtemplate');?>
+                            <div class="form-group row">
+                                <div class="col-sm-3">Upload Template</div>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+
                 </div>
                 <!-- /.container-fluid -->
             </div>

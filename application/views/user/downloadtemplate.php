9a8cba24702883619e57f5923253fed20aab044e
Ednagea Almira <ednagea.almira@gmail.com>
2021-02-10 05:38:23 +0700

    minor update

--- application/views/user/downloadtemplate.php
+++ application/views/user/downloadtemplate.php
@@ -6,7 +6,7 @@
 
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Daftar Pengguna</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Daftar Template</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">

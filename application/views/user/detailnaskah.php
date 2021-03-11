998388b9544c98894f4bf6ed24952b0c943de31d
Ednagea Almira <ednagea.almira@gmail.com>
2021-03-11 23:05:14 +0700

    adding to detailnaskah

--- application/views/user/detailnaskah.php
+++ application/views/user/detailnaskah.php
@@ -4,6 +4,12 @@
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+                    <div class="card mb-4 py-3 border-left-primary">
+                        <div class="card-body">
+                            this is content
+                        </div>
+                    </div>
+
                 </div>
                 <!-- /.container-fluid -->
             </div>

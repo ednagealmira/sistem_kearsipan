bbf5fc8d0323f4f698a4c563b3f590dad02bf07e
Ednagea Almira <ednagea.almira@gmail.com>
2021-03-11 15:51:55 +0700

    Update role.php

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -1,34 +1,40 @@
 <div class="container-fluid">
     
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-    <div class="row">
-        <div class="col-lg-6">
-            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
-            <?= $this->session->flashdata('message'); ?>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Role</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($role as $r) : ?>
-                    <tr>
-                        <th scope="row"><?= $i; ?></th>
-                        <td><?= $r['role']; ?></td>
-                        <td>
-                            <a href="<?= base_url('admin/roleaccess/') . $r['id']; ?>" class="btn btn-success btn-circle btn-sm btn-edit">
-                                <i class="fas fa-fw fa-tasks"></i>
-                            </a>
-                        </td>
-                    </tr>
-                    <?php $i++; ?>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+
+    <?= $this->session->flashdata('message'); ?>
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3 bg-gradient-primary">
+            <h6 class="m-0 font-weight-bold text-white text-center">Daftar Pengguna</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        <?php foreach ($role as $r) : ?>
+                        <tr>
+                            <th scope="row"><?= $i; ?></th>
+                            <td><?= $r['role']; ?></td>
+                            <td>
+                                <a href="<?= base_url('admin/roleaccess/') . $r['id']; ?>" class="btn btn-success btn-circle btn-sm btn-edit">
+                                    <i class="fas fa-fw fa-tasks"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>

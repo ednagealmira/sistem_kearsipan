b8055412d544efb69aafe9c2f84eb356d7ee99b5
Ednagea Almira <ednagea.almira@gmail.com>
2021-03-11 16:19:46 +0700

    update

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -6,7 +6,7 @@
 
     <div class="card shadow mb-4">
         <div class="card-header py-3 bg-gradient-primary">
-            <h6 class="m-0 font-weight-bold text-white text-center">Daftar Pengguna</h6>
+            <h6 class="m-0 font-weight-bold text-white text-center">Daftar Role</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
--- application/views/admin/role_access.php
+++ application/views/admin/role_access.php
@@ -1,34 +1,39 @@
 <div class="container-fluid">
     
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-    <div class="row">
-        <div class="col-lg-6">
-            <?= $this->session->flashdata('message'); ?>
-            <h5 class="font-weight-light">Role : <?= $role['role']; ?></h5>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Menu</th>
-                        <th scope="col">Access</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($listmenu as $m) : ?>
-                    <tr>
-                        <th scope="row"><?= $i; ?></th>
-                        <td><?= $m['menu']; ?></td>
-                        <td>
-                            <div class="form-check">
-                                <input class="form-check-input check-input-role" type="checkbox" <?= check_access($role['id'], $m['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $m['id']; ?>">
-                            </div>
-                        </td>
-                    </tr>
-                    <?php $i++; ?>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+        <?= $this->session->flashdata('message'); ?>
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3 bg-gradient-primary">
+            <h6 class="m-0 font-weight-bold text-white text-center">Role : <?= $role['role']; ?></h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Menu</th>
+                            <th scope="col">Access</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        <?php foreach ($listmenu as $m) : ?>
+                        <tr>
+                            <th scope="row"><?= $i; ?></th>
+                            <td><?= $m['menu']; ?></td>
+                            <td>
+                                <div class="form-check">
+                                    <input class="form-check-input check-input-role" type="checkbox" <?= check_access($role['id'], $m['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $m['id']; ?>">
+                                </div>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>

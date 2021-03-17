0b41076048e0b0806a0b8c4d533b15f693d34042
Ednagea Almira <ednagea.almira@gmail.com>
2021-03-17 07:27:10 +0700

    Update index.php

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -3,7 +3,7 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
+                    <p class="mr-1">Selamat datang di Sistem Informasi Kearsipan Dinas Kearsipan dan Perpustakaan Kota Palembang.</p>
                 </div>
                 <!-- /.container-fluid -->
             </div>
